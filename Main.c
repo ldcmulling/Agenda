10f2d26cdd2622ab7e9f36d0eb8d7c9c4b587d29
Lourenço <48692097+ldcmulling@users.noreply.github.com>
2019-04-07 21:45:38 -0300

    Update Main.c

--- Main.c
+++ Main.c
@@ -13,12 +13,14 @@ int idade, telefone;
 
 void adicionar(){
 	printf("Digite o Nome \n");
-	scanf("%s", & pet[*cp].nome[20]);
+	scanf("%s", & (pet+*cp)->nome);
+//	getchar();
 	printf("Digite a idade\n");
-	scanf("%d", & pet[*cp].idade);	
+	scanf("%d", & (pet+*cp)->idade);
 	printf("Digite o Telefone\n");
-	scanf("%d", & pet[*cp].telefone);
+	scanf("%d", & (pet+*cp)->telefone);
 }
+
 
 void realocar (){
 	buffer = realloc (buffer , sizeof (struct pessoa)*(*cp));
@@ -26,28 +28,65 @@ void realocar (){
 	c=cp+1;
 	a= (float*)c+1;
 	pet=(struct pessoa*)a+1;
+	printf (" Realioc OK \n");
 
 }
 
 void imprimirU (){
-	
-	
-}
 
-void apagar (){
-	
-	
+
 }
 
 void imprimirT (){
-	for(*c=0;(*c)<=(*cp);(*c)++){
-		printf("%s", & pet[*c].nome[20]);
-		printf("%d", pet[*c].idade);
-		printf("%d", pet[*c].telefone);
-
+	for(int x=0;x<(*cp);x++){
+		printf("Nome %s\n", (pet+x)->nome);
+		printf("Idade %d\n", (pet+x)->idade);
+		printf("Telefone %d\n", (pet+x)->telefone);
 	}
-	
 }
+
+int main(int argc, char **argv){
+
+    buffer=malloc((2*sizeof(int))+sizeof(float)+sizeof(struct pessoa));
+	cp= (int*) buffer;
+	c=cp+1;
+	a= (float*)c+1;
+	pet=(struct pessoa*)a+1;
+	*cp=0;
+
+	//Menu
+	for(*c=20;*c!=0;){
+		printf (" Digite 1 para adicionar uma pessoa \n");
+		printf (" Digite 2 para imprimir uma pessoa\n");
+		printf (" Digite 3 para imprimir tudo\n");
+		printf (" Digite 4 para apagar o ultimo\n");
+		printf (" Digite 0 para sair \n");
+		scanf("%d",c);
+//Adiciona UN
+		if ( *c == 1 ) {
+			//realocar ();
+			adicionar();
+			*cp=*cp+1;
+		}
+//Imprime UN
+		else if ((*c) == 2) {imprimirU ();}
+//Imprime tudo
+		else if ( (*c) == 3 ) {imprimirT ();}
+//Apagar o ultimo
+		else if ( *c == 4 ) {
+			*cp=*cp-1;
+			realocar ();
+			;
+		}
+//Sair
+		else if ( *c == 0 ) { printf ("voce saiu");}
+		else{printf ("numero errado tente denovo\n");}
+	}
+	free(buffer);
+
+	return 0;
+}
+
 
 int main(int argc, char **argv){
 
