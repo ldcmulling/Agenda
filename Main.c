10467f0e504a1846f571acf1553bbb3b57351390
Lourenço <48692097+ldcmulling@users.noreply.github.com>
2019-04-08 21:37:40 -0300

    Update Main.c

--- Main.c
+++ Main.c
@@ -13,8 +13,7 @@ int idade, telefone;
 
 void adicionar(){
 	printf("Digite o Nome \n");
-	scanf("%s", & (pet+*cp)->nome);
-//	getchar();
+	scanf("%s", (pet+*cp)->nome);
 	printf("Digite a idade\n");
 	scanf("%d", & (pet+*cp)->idade);
 	printf("Digite o Telefone\n");
@@ -23,19 +22,18 @@ void adicionar(){
 
 
 void realocar (){
-	buffer = realloc (buffer , sizeof (struct pessoa)*(*cp));
-	cp=buffer;
-	c=cp+1;
+    void *aux;
+    aux=buffer;
+	buffer = (void*)realloc (buffer , (sizeof (struct pessoa))*(*cp));
+	cp=(int*)aux;
+	c=(int*)cp+1;
 	a= (float*)c+1;
 	pet=(struct pessoa*)a+1;
+	buffer = aux;
 	printf (" Realioc OK \n");
-
 }
 
-void imprimirU (){
-
-
-}
+void imprimirU (){}
 
 void imprimirT (){
 	for(int x=0;x<(*cp);x++){
@@ -56,7 +54,7 @@ int main(int argc, char **argv){
 
 	//Menu
 	for(*c=20;*c!=0;){
-		printf (" Digite 1 para adicionar uma pessoa \n");
+		printf (" Digite 1 para adicionar uma pessoa \n Digite 2 para imprimir uma pessoa\n");
 		printf (" Digite 2 para imprimir uma pessoa\n");
 		printf (" Digite 3 para imprimir tudo\n");
 		printf (" Digite 4 para apagar o ultimo\n");
@@ -64,7 +62,9 @@ int main(int argc, char **argv){
 		scanf("%d",c);
 //Adiciona UN
 		if ( *c == 1 ) {
-			//realocar ();
+            (*cp)++;
+            realocar ();
+            (*cp)--;
 			adicionar();
 			*cp=*cp+1;
 		}
@@ -76,48 +76,12 @@ int main(int argc, char **argv){
 		else if ( *c == 4 ) {
 			*cp=*cp-1;
 			realocar ();
-			;
 		}
 //Sair
 		else if ( *c == 0 ) { printf ("voce saiu");}
 		else{printf ("numero errado tente denovo\n");}
 	}
 	free(buffer);
-
-	return 0;
-}
-
-
-int main(int argc, char **argv){
-
-    buffer=malloc((2*sizeof(int))+sizeof(float)+sizeof(struct pessoa));
-	cp=buffer;
-	c=cp+1;
-	a= (float*)c+1;
-	pet=(struct pessoa*)a+1;
-	*cp=0;
-
-	//Menu
-	for(*c=20;*c!=0;){
-		printf (" Digite 1 para adicionar uma pessoa \n");
-		printf (" Digite 2 para imprimir uma pessoa\n");
-		printf (" Digite 3 para imprimir tudo\n");
-		printf (" Digite 4 para apagar o ultimo\n");
-		printf (" Digite 0 para sair \n");
-		scanf("%d",c);
-		if ( *c == 1 ) { 
-			realocar ();
-			adicionar();
-			*cp=*cp+1;
-		}
-		if ( *c == 2 ) {imprimirU ();}
-		if ( *c == 3 ) {imprimirT ();}
-		if ( *c == 4 ) {apagar ();}
-		if ( *c == 0 ) { printf ("voce saiu");}
-
-		else{printf ("numero errado tente denovo\n");}
-	}
-	free (buffer);
 
 	return 0;
 }
