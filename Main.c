bf24630ac91e9ae3b469c50ef12cfbdb7b602b4a
Lourenço <48692097+ldcmulling@users.noreply.github.com>
2019-04-12 11:52:16 -0300

    Update Main.c
    realloc corrigido.

--- Main.c
+++ Main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 
 void *buffer;
 int *cp, *c;
-float *a;
 struct pessoa *pet;
 
 struct pessoa{
@@ -20,20 +21,29 @@ void adicionar(){
 	scanf("%d", & (pet+*cp)->telefone);
 }
 
-
 void realocar (){
-    void *aux;
-    aux=buffer;
-	buffer = (void*)realloc (buffer , (sizeof (struct pessoa))*(*cp));
-	cp=(int*)aux;
-	c=(int*)cp+1;
-	a= (float*)c+1;
-	pet=(struct pessoa*)a+1;
-	buffer = aux;
+	buffer = (void*)realloc (buffer , (2*sizeof(int)+ sizeof (struct pessoa))*(*cp+1));
+	cp=(int*)buffer;
+	c=cp+1;
+	pet=(struct pessoa*)c+1;
 	printf (" Realioc OK \n");
 }
 
-void imprimirU (){}
+void delete(){
+	char temp[20];
+	printf (" Digite o nome da pessoa\n");
+	scanf(" %s" , temp);
+	for(int x=0;x<*cp;x++){
+		if (strcmp(temp,(pet+x)->nome) == 0){
+			
+		
+		x=*cp;
+		}
+		else {printf("Nome nao encontrado\n\n");}
+	}
+	printf("Nome %s\n", temp);
+	
+}
 
 void imprimirT (){
 	for(int x=0;x<(*cp);x++){
@@ -45,33 +55,32 @@ void imprimirT (){
 
 int main(int argc, char **argv){
 
-    buffer=malloc((2*sizeof(int))+sizeof(float)+sizeof(struct pessoa));
+    buffer=malloc((2*sizeof(int))+sizeof(struct pessoa));
 	cp= (int*) buffer;
-	c=cp+1;
-	a= (float*)c+1;
-	pet=(struct pessoa*)a+1;
+	c=(int*)cp+1;
+	pet=(struct pessoa*)c+1;
 	*cp=0;
 
 	//Menu
 	for(*c=20;*c!=0;){
-		printf (" Digite 1 para adicionar uma pessoa \n Digite 2 para imprimir uma pessoa\n");
-		printf (" Digite 2 para imprimir uma pessoa\n");
-		printf (" Digite 3 para imprimir tudo\n");
+		printf (" Digite 1 para adicionar uma pessoa\n");
+		printf (" Digite 2 para imprimir tudo\n");
+		printf (" Digite 3 para apagar uma pessoa\n");
 		printf (" Digite 4 para apagar o ultimo\n");
 		printf (" Digite 0 para sair \n");
 		scanf("%d",c);
 //Adiciona UN
 		if ( *c == 1 ) {
-            (*cp)++;
             realocar ();
-            (*cp)--;
 			adicionar();
 			*cp=*cp+1;
 		}
-//Imprime UN
-		else if ((*c) == 2) {imprimirU ();}
 //Imprime tudo
-		else if ( (*c) == 3 ) {imprimirT ();}
+		else if ( (*c) == 2 ) {imprimirT ();}
+//Pesquisar e Apagar
+		else if ( *c == 3 ) {
+			delete();
+		}
 //Apagar o ultimo
 		else if ( *c == 4 ) {
 			*cp=*cp-1;
